6de5b6035b42f9e1324446711c49b5b8de826992
Matthieu Nadeau <matthieunadeau96@gmail.com>
2020-09-28 13:32:33 -0400

    Added graphic design assets & started grid for Work page

--- src/pages/Work.js
+++ src/pages/Work.js
@@ -1,12 +1,43 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
+import { Grid, Paper } from '@material-ui/core';
+
+const useStyles = makeStyles((theme) => ({
+    grid: {
+        width: '100%',
+        margin: '0px',
+    },
+    paper: {
+        padding: theme.spacing(2),
+        textAlign: 'center',
+        color: theme.palette.text.secondary,
+        background: theme.palette.info.light,
+    }
+}));
 
 
 export default function Home() {
+    const classes = useStyles();
     return (
         <div>
             <Link to="/"><div><h3>Back</h3></div></Link>
             <h1>Work</h1>
+            <Grid container spacing={2} className={classes.grid}>
+                <Grid item xs={12} md={6} lg={3}>
+                    <Paper className={classes.paper}>1</Paper>
+                </Grid>
+                <Grid item xs={12} md={6} lg={3}>
+                    <Paper className={classes.paper}>1</Paper>
+                </Grid>
+                <Grid item xs={12} md={6} lg={3}>
+                    <Paper className={classes.paper}>1</Paper>
+                </Grid>
+
+                <Grid item xs={12} md={6} lg={3}>
+                    <Paper className={classes.paper}>1</Paper>
+                </Grid>
+            </Grid>
         </div>
     );
 }
